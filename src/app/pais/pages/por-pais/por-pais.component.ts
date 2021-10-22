643a8087a0b51f282a4c71adec929d7e948f7a44
mariajbellido <mariajbellido@gmail.com>
2021-10-22 10:47:13 +0200

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -9,12 +9,22 @@ import { PaisService } from '../services/pais.service';
 export class PorPaisComponent  {
 
   termino: string = '';
+  hayError: boolean = false;
+  
 
   constructor( private paisService: PaisService) { }
 
   buscar() {
+
+    this.hayError = false;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino).subscribe(response => {console.log(response)});
+    this.paisService.buscarPais(this.termino)
+    .subscribe((resp) => {
+      
+      console.log(resp);
+    }, (error) => {
+      this.hayError = true;
+    });
   }
 }
