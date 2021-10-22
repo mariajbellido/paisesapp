5eda305df835278d17ede330015d25b2e9105431
mariajbellido <mariajbellido@gmail.com>
2021-10-22 11:29:15 +0200

    Llenando la tabla de países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../services/pais.service';
 
+import { Country } from '../../interfaces/pais.interface';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -10,6 +12,7 @@ export class PorPaisComponent  {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
   
 
   constructor( private paisService: PaisService) { }
@@ -21,10 +24,11 @@ export class PorPaisComponent  {
 
     this.paisService.buscarPais(this.termino)
     .subscribe((paises) => {
-      
       console.log(paises);
+      this.paises = paises;
     }, (error) => {
       this.hayError = true;
+      this.paises = [];
     });
   }
 }
