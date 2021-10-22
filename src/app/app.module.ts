a58e75e3a82142862f94519180ac9126e57462b7
mariajbellido <mariajbellido@gmail.com>
2021-10-22 10:16:01 +0200

    Trabajando con servicio para buscar países

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -9,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [ AppComponent ],
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule, SharedModule, PaisModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, SharedModule, PaisModule, HttpClientModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
