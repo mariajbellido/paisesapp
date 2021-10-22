99ad4201b5ba6a8786fbd0216f46b0baff3c46bc
mariajbellido <mariajbellido@gmail.com>
2021-10-22 09:35:13 +0200

    Trabajando con componente para buscar por país

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorRegionComponent,
     VerPaisComponent
   ],
-  imports: [CommonModule],
+  imports: 
+  [CommonModule,
+  FormsModule],
 })
 export class PaisModule {}
