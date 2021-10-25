72b2c4a8cc92d427d2f0900ed843c7a1209a3d60
mariajbellido <mariajbellido@gmail.com>
2021-10-25 08:46:11 +0200

    Trabajando en por capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
  
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent  {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  
 
-  ngOnInit() {
+  constructor( private paisService: PaisService) { }
+
+  buscar( termino: string ) {
+
+    this.hayError = false;
+    this.termino = termino
+   // console.log(this.termino);
+
+    this.paisService.buscarCapital(this.termino)
+    .subscribe((paises) => {
+      console.log(paises);
+      this.paises = paises;
+    }, (error) => {
+      this.hayError = true;
+      this.paises = [];
+    });
   }
+
 
 }
